a9d554a899a20a9bb3eb24dde81520466bd5405a
Bryan Silva <bryanstos99@gmail.com>
2026-04-08 15:15:28 -0300

    Project created

---
+++ src/test/java/com/project/roomservation/RoomservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.roomservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
